8d8b910e0db85a140e84e48cc9afaf8bbba9a23e
cassaundra <cass@cassaundra.org>
2023-02-25 02:39:17 -0800

    Add working prototype
    Working so far:
    - Uploading and downloading files to/from S3
    - Handling errors and representing them to the user
    - Getting config from environment and config.toml
    - Generating IDs

---
+++ src/error.rs
@@ -0,0 +1,81 @@
+use aws_sdk_s3 as s3;
+use axum::extract::multipart::MultipartError;
+use axum::http::{self, StatusCode};
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use s3::types::SdkError;
+use serde::Serialize;
+use thiserror::Error;
+
+pub type ApiResult<T> = std::result::Result<T, ApiError>;
+
+#[derive(Error, Debug)]
+#[non_exhaustive]
+pub enum ApiError {
+    #[error("paste not found")]
+    PasteNotFound,
+    #[error("missing multipart file")]
+    MissingFile,
+    #[error("missing multipart file name")]
+    MissingFileName,
+    #[error("missing content type for multipart file")]
+    MissingFileContentType,
+    #[error("error reading multipart data")]
+    Multipart {
+        #[from]
+        source: MultipartError,
+    },
+    #[error("http error")]
+    Http {
+        #[from]
+        source: http::Error,
+    },
+    #[error("other error")]
+    Other {
+        source: Box<dyn std::error::Error + Send + Sync + 'static>,
+    },
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        let status_code = match &self {
+            ApiError::PasteNotFound => StatusCode::NOT_FOUND,
+            ApiError::MissingFile => StatusCode::BAD_REQUEST,
+            ApiError::MissingFileName => StatusCode::BAD_REQUEST,
+            ApiError::MissingFileContentType => StatusCode::BAD_REQUEST,
+            ApiError::Multipart { .. } => StatusCode::BAD_REQUEST,
+            ApiError::Http { .. } => StatusCode::BAD_REQUEST,
+            ApiError::Other { .. } => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        let response = ErrorInfo {
+            message: format!("{self}"),
+        };
+        (status_code, Json(response)).into_response()
+    }
+}
+
+impl From<SdkError<s3::error::GetObjectError>> for ApiError {
+    fn from(source: SdkError<s3::error::GetObjectError>) -> Self {
+        let error = source.into_service_error();
+        match error.kind {
+            s3::error::GetObjectErrorKind::NoSuchKey(_) => ApiError::PasteNotFound,
+            _ => ApiError::Other {
+                source: Box::new(error),
+            },
+        }
+    }
+}
+
+impl From<SdkError<s3::error::PutObjectError>> for ApiError {
+    fn from(value: SdkError<s3::error::PutObjectError>) -> Self {
+        ApiError::Other {
+            source: Box::new(value),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct ErrorInfo {
+    message: String,
+}
---
+++ src/main.rs
@@ -0,0 +1,180 @@
+use std::io::BufRead;
+use std::net::SocketAddr;
+use std::path::PathBuf;
+use std::{fs, io};
+
+use anyhow::Context;
+use aws_config::retry::RetryConfig;
+use aws_sdk_s3 as s3;
+use axum::body::Bytes;
+use axum::extract::{DefaultBodyLimit, FromRef, Multipart, Path, State};
+use axum::response::{Redirect, Response};
+use axum::routing::get;
+use axum::{body, Router};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use serde::Deserialize;
+use tower_http::limit::RequestBodyLimitLayer;
+use tracing::info;
+use urlencoding::encode;
+
+mod error;
+pub(crate) use error::{ApiError, ApiResult};
+
+#[derive(Clone, FromRef)]
+struct AppState {
+    config: Config,
+    words: Words,
+    s3_client: s3::Client,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Config {
+    port: u16,
+    max_upload_size: usize,
+    adjectives_file: PathBuf,
+    nouns_file: PathBuf,
+    s3_bucket: String,
+    s3_region: String,
+    s3_endpoint: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+struct Words {
+    adjectives: Vec<String>,
+    nouns: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let config: Config = config::Config::builder()
+        .add_source(config::File::with_name("config.toml").required(false))
+        .add_source(config::Environment::with_prefix("NONBIN"))
+        .build()
+        .context("failed to read config")?
+        .try_deserialize()
+        .context("failed to deserialize config")?;
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.port));
+
+    let words = Words {
+        adjectives: read_lines(&config.adjectives_file).context("failed to read adjectives")?,
+        nouns: read_lines(&config.nouns_file).context("failed to read nouns")?,
+    };
+
+    let s3_client = {
+        let sdk_config = aws_config::load_from_env().await;
+        let mut config_builder = s3::config::Builder::from(&sdk_config)
+            .region(s3::Region::new(config.s3_region.clone()))
+            .retry_config(RetryConfig::disabled());
+
+        if let Some(endpoint) = &config.s3_endpoint {
+            config_builder = config_builder.endpoint_url(endpoint);
+        }
+
+        let s3_config = config_builder.build();
+
+        s3::Client::from_conf(s3_config)
+    };
+
+    let app = Router::new()
+        .route("/", get(index).post(upload_paste))
+        .route("/:id/:file_name", get(get_paste))
+        .layer(DefaultBodyLimit::disable())
+        .layer(RequestBodyLimitLayer::new(config.max_upload_size))
+        .with_state(AppState {
+            config,
+            words,
+            s3_client,
+        });
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+async fn index() -> &'static str {
+    "TODO documentation"
+}
+
+async fn get_paste(
+    State(config): State<Config>,
+    State(s3_client): State<s3::Client>,
+    Path((id, _file_name)): Path<(String, String)>,
+) -> crate::ApiResult<Response<body::Full<Bytes>>> {
+    let object = s3_client
+        .get_object()
+        .bucket(&config.s3_bucket)
+        .key(&id)
+        .send()
+        .await?;
+
+    let body = object.body.collect().await.unwrap().into_bytes();
+    let response = Response::builder().body(body::Full::new(body))?;
+
+    Ok(response)
+}
+
+async fn upload_paste(
+    State(config): State<Config>,
+    State(words): State<Words>,
+    State(s3_client): State<s3::Client>,
+    mut multipart: Multipart,
+) -> crate::ApiResult<Redirect> {
+    // just take the first multipart field
+    if let Some(field) = multipart.next_field().await? {
+        let file_name = field
+            .file_name()
+            .ok_or_else(|| ApiError::MissingFileName)?
+            .to_owned();
+        let content_type = field
+            .content_type()
+            .ok_or_else(|| ApiError::MissingFileContentType)?
+            .to_owned();
+        let data = field.bytes().await?;
+
+        let id = generate_id(&words);
+
+        info!(
+            "uploading: id='{id}', file='{file_name}', content_type='{content_type}', size={size}",
+            size = data.len()
+        );
+
+        s3_client
+            .put_object()
+            .bucket(&config.s3_bucket)
+            .key(&id)
+            .metadata("user-file-name", &file_name)
+            .metadata("user-content-type", &content_type)
+            .body(data.into())
+            .send()
+            .await?;
+
+        let encoded_file_name = encode(&file_name);
+        Ok(Redirect::to(&format!("/{id}/{encoded_file_name}")))
+    } else {
+        Err(ApiError::MissingFile)
+    }
+}
+
+fn generate_id(words: &Words) -> String {
+    let mut rng = thread_rng();
+    let adj_a = words.adjectives.choose(&mut rng).unwrap();
+    let adj_b = words.adjectives.choose(&mut rng).unwrap();
+    let noun = words.nouns.choose(&mut rng).unwrap();
+    format!("{adj_a}-{adj_b}-{noun}")
+}
+
+fn read_lines(path: impl AsRef<std::path::Path>) -> io::Result<Vec<String>> {
+    let file = fs::File::open(path)?;
+    let lines = io::BufReader::new(file)
+        .lines()
+        .filter_map(|s| s.ok())
+        .filter(|s| !s.is_empty())
+        .collect();
+    Ok(lines)
+}
